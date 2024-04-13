4c91dd955f92464bde3a8313374c92f12cff2bdf
mdd3135 <313521996@qq.com>
2024-04-13 11:25:43 +0800

    完成findAllStaff和modUser接口

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
-import com.example.demo.mapper.UserMapper;
 import com.example.demo.service.UserService;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 public class UserController {
@@ -19,10 +19,26 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public User login(@RequestParam Map<String,String>map) {
+    public User login(@RequestParam Map<String, String> map) {
         String username = map.get("username");
         String password = map.get("password");
         return userService.login(username, password);
     }
-    
+
+    @GetMapping("/findAllStaff")
+    public List<User> findAllStaff() {
+        return userService.findAllStaff();
+    }
+
+    @PostMapping("/modUser")
+    public User modUser(@RequestParam Map<String, String> map) {
+        int userid = Integer.parseInt(map.get("userid"));
+        String username = map.get("username");
+        String password = map.get("password");
+        String phone_number = map.get("phone_number");
+        String region = map.get("region");
+        int onjob = Integer.parseInt(map.get("onjob"));
+        int type = Integer.parseInt(map.get("type"));
+        return userService.modUser(userid, username, password, phone_number, region, onjob, type);
+    }
 }
--- demo/src/main/java/com/example/demo/mapper/UserMapper.java
+++ demo/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -9,32 +9,24 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.entity.User;
+
 @Mapper
 public interface UserMapper {
     @Select("select * from user;")
     List<User> findAllUser();
 
+    @Select("select * from user where type=0")
+    List<User> findAllStaff();
+
     @Select("select * from user where username=#{username};")
     User findUserByName(String username);
-
-    @Update("update user set password=#{password} where username=#{username};")
-    void modPassword(String username, String password);
-
-    @Select("select * from user where type=0;")
-    List<User> findAllStu();
 
     @Insert("insert into user (username, password, phone_number, region,onjob, type) values (#{username}, #{password}, #{phone_nbumber}, #{region}, #{onjob},#{type});")
     void addUser(User user);
 
-    @Delete("delete from user where type=0;")
-    void delAllStu();
-
     @Delete("delete from user where username=#{username};")
     void delUserByName(String user);
 
-    @Update("update user set region=#{region} where username=#{username};")
-    void modClassroom(String username, String region);
-
-    @Update("update user set phone_number=#{phone_number} where username=#{username};")
-    void modNumber(String username, String phone_number);
+    @Update("update user set username=#{username}, password=#{password}, phone_number=#{phone_number}, region=#{region}, onjob=#{onjob}, type=#{type} where userid=#{userid}")
+    void modUser(User user);
 }
